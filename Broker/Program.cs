d5c6752ff6890671b4c4c99c21c1242044e6be48
eeron.john.pascual <eeron.john.pascual@accenture.com>
2021-02-01 11:15:46 +0800

    Update 2-1

--- Broker/Program.cs
+++ Broker/Program.cs
@@ -8,11 +8,15 @@ using MQTTnet.Protocol;
 using MQTTnet.Server;
 using System.Net;
 using System.Net.Sockets;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+using System.Runtime.Loader;
 
 namespace ConsoleBroker
 {
     class Program
     {
+        static int counter;
          public static string GetLocalIPAddress()
         {
             var host = Dns.GetHostEntry(Dns.GetHostName());
@@ -27,7 +31,25 @@ namespace ConsoleBroker
         }
         static void Main(string[] args)
         {
-            //configure options
+           Init().Wait();
+
+            // Wait until the app unloads or is cancelled
+            var cts = new CancellationTokenSource();
+            AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
+            Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
+            WhenCancelled(cts.Token).Wait();
+
+        }
+
+        public static Task WhenCancelled(CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            return tcs.Task;
+        }
+        static async Task Init()
+        {
+              //configure options
             var optionsBuilder = new MqttServerOptionsBuilder()
                 .WithConnectionValidator(c =>
                 {
@@ -49,7 +71,7 @@ namespace ConsoleBroker
             //start server
             var mqttServer = new MqttFactory().CreateMqttServer();
             mqttServer.StartAsync(optionsBuilder.Build()).Wait();
-//mqttServer.Options.DefaultEndpointOptions.BoundInterNetworkAddress
+            //mqttServer.Options.DefaultEndpointOptions.BoundInterNetworkAddress
             Console.WriteLine($"Broker is Running: Host: {GetLocalIPAddress()} Port: {mqttServer.Options.DefaultEndpointOptions.Port}");
             Console.WriteLine("Press any key to exit.");
             Console.ReadLine();
@@ -60,7 +82,45 @@ namespace ConsoleBroker
 
             mqttServer.StopAsync().Wait();
 
+            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
+            ITransportSettings[] settings = { mqttSetting };
 
+            // Open a connection to the Edge runtime
+            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            await ioTHubModuleClient.OpenAsync();
+            Console.WriteLine("IoT Hub module client initialized.");
+
+            // Register callback to be called when a message is received by the module
+            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
+        }
+          static async Task<MessageResponse> PipeMessage(Message message, object userContext)
+        {
+            int counterValue = Interlocked.Increment(ref counter);
+
+            var moduleClient = userContext as ModuleClient;
+            if (moduleClient == null)
+            {
+                throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
+            }
+
+            byte[] messageBytes = message.GetBytes();
+            string messageString = Encoding.UTF8.GetString(messageBytes);
+            Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
+
+            if (!string.IsNullOrEmpty(messageString))
+            {
+                using (var pipeMessage = new Message(messageBytes))
+                {
+                    foreach (var prop in message.Properties)
+                    {
+                        pipeMessage.Properties.Add(prop.Key, prop.Value);
+                    }
+                    await moduleClient.SendEventAsync("output1", pipeMessage);
+                
+                    Console.WriteLine("Received message sent");
+                }
+            }
+            return MessageResponse.Completed;
         }
     }
 }
